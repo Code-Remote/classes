1f582d129ff22de76d0241687e4615f05a17cb08
Code-Remote <evert.van.de.braak@ing.com>
2020-02-14 07:49:16 +0100

    Add Constraint layout and modify Manifest

---
+++ app/src/main/java/com/code_remote/classes/class2/MainActivity00ConstraintLayout.kt
@@ -0,0 +1,13 @@
+package com.code_remote.classes.class2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.code_remote.classes.R
+
+class MainActivity00ConstraintLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_constraint_layout)
+    }
+}
